aa677371396bc56cef795cdaa0c02371af93eb58
Jip Irfandy <irfandyjip98@gmail.com>
2024-05-18 16:39:31 +0700

    feat(api): add ability to get rooms

--- api/src/handlers/rooms.ts
+++ api/src/handlers/rooms.ts
@@ -26,101 +26,106 @@ interface RoomDoc extends Document {
   messages: ObjectId[];
 }
 
+interface IndexRoomDto {
+  id: string;
+  name: string;
+  description: string;
+}
+
 /**
  * A simple example includes a HTTP get method.
  */
-// export const getUsersHandler = async (
-//   event: APIGatewayProxyEvent,
-// ): Promise<APIGatewayProxyResult> => {
-//   // All log statements are written to CloudWatch
-//   console.debug('Received event:', event);
+export const getRoomsHandler = async (
+  event: APIGatewayProxyEvent,
+): Promise<APIGatewayProxyResult> => {
+  // All log statements are written to CloudWatch
+  console.debug('Received event:', event);
 
-//   try {
-//     const mongoClient = await getMongoClient();
-//     if (!mongoClient || !mongoClient.db || !mongoClient.closeConnection) {
-//       console.log("500 - GET /users - Error connecting to DB")
-//       return {
-//         statusCode: 500,
-//         body: JSON.stringify({ message: 'Internal Server Error' })
-//       };
-//     }
-//     const { db, closeConnection } = mongoClient;
+  try {
+    const mongoClient = await getMongoClient();
+    if (!mongoClient || !mongoClient.db || !mongoClient.closeConnection) {
+      console.log("500 - GET /rooms - Error connecting to DB")
+      return {
+        statusCode: 500,
+        body: JSON.stringify({ message: 'Internal Server Error' })
+      };
+    }
+    const { db, closeConnection } = mongoClient;
 
-//     // Are there any queries
-//     const qs = event.queryStringParameters;
-//     const query: Query = {
-//       limit: 10,
-//       page: 1,
-//       sortBy: ['name', 'ASC'],
-//       filter: {}
-//     }
-//     if (qs) {
-//       query.limit = qs.limit ? parseInt(qs.limit) : 10;
-//       query.page = qs.page ? parseInt(qs.page) : 1;
+    // Are there any queries
+    const qs = event.queryStringParameters;
+    const query: Query = {
+      limit: 10,
+      page: 1,
+      sortBy: ['name', 'ASC'],
+      filter: {}
+    }
+    if (qs) {
+      query.limit = qs.limit ? parseInt(qs.limit) : 10;
+      query.page = qs.page ? parseInt(qs.page) : 1;
 
-//       // Sort by name or email
-//       // Configure this later
-//       // query.sortBy = qs.sortBy ? qs.sortBy : ['name', 'ASC']
+      // Configure this later
+      // query.sortBy = qs.sortBy ? qs.sortBy : ['name', 'ASC']
 
-//       // Filter where name or email contains the query string
-//       query.filter = qs.filter ? {
-//         $or: [
-//           { name: { $regex: qs.filter, $options: 'i' } },
-//           { email: { $regex: qs.filter, $options: 'i' } }
-//         ]
-//       } : {}
-//     }
+      // Filter where name or email contains the query string
+      query.filter = qs.filter ? {
+        $or: [
+          { name: { $regex: qs.filter, $options: 'i' } },
+          { email: { $regex: qs.filter, $options: 'i' } }
+        ]
+      } : {}
+    }
 
-//     // Get the users collection
-//     const usersCollection = db.collection<RoomDoc>('users');
+    // Get the users collection
+    const roomsCollection = db.collection<RoomDoc>(Entities.ROOMS);
 
-//     // Get all users
-//     const users = await usersCollection.find<RoomDoc>(query.filter, {
-//       limit: query.limit,
-//       skip: (query.page - 1) * query.limit,
-//       sort: {
-//         [query.sortBy[0]]: query.sortBy[1] === 'ASC' ? 1 : -1
-//       }
-//     }).toArray();
+    // Get all users
+    const rooms = await roomsCollection.find<RoomDoc>(query.filter, {
+      limit: query.limit,
+      skip: (query.page - 1) * query.limit,
+      sort: {
+        [query.sortBy[0]]: query.sortBy[1] === 'ASC' ? 1 : -1
+      }
+    }).toArray();
 
-//     const totalDocuments = await usersCollection.countDocuments(query.filter);
-//     const totalPages = Math.ceil(totalDocuments / query.limit);
-//     const usersResponse: GetResponse<User> = {
-//       data: users.map(user => ({
-//         id: user.id,
-//         name: user.name,
-//         email: user.email
-//       })),
-//       meta: {
-//         totalItems: totalDocuments,
-//         currentPage: query.page,
-//         totalPages: totalPages,
-//         sortBy: [['name', 'ASC']],
-//         limit: query.limit,
-//       },
-//       links: {
-//         first: `${CLIENT_HOST}/users?limit=${query.limit}&page=1`,
-//         previous: query.page > 1 ? `${CLIENT_HOST}/users?limit=${query.limit}&page=${query.page - 1}` : '',
-//         current: `${CLIENT_HOST}/users?limit=${query.limit}&page=${query.page}`,
-//         next: query.page < totalPages ? `${CLIENT_HOST}/users?limit=${query.limit}&page=${query.page + 1}` : '',
-//         last: `${CLIENT_HOST}/users?limit=${query.limit}&page=${totalPages}`,
-//       }
-//     }
+    const totalDocuments = await roomsCollection.countDocuments(query.filter);
+    const totalPages = Math.ceil(totalDocuments / query.limit);
+    const roomsResponse: GetResponse<IndexRoomDto> = {
+      data: rooms.map(room => ({
+        id: room.id,
+        name: room.name,
+        description: room.description
+      })),
+      meta: {
+        totalItems: totalDocuments,
+        currentPage: query.page,
+        totalPages: totalPages,
+        sortBy: [['name', 'ASC']],
+        limit: query.limit,
+      },
+      links: {
+        first: `${CLIENT_HOST}/rooms?limit=${query.limit}&page=1`,
+        previous: query.page > 1 ? `${CLIENT_HOST}/rooms?limit=${query.limit}&page=${query.page - 1}` : '',
+        current: `${CLIENT_HOST}/rooms?limit=${query.limit}&page=${query.page}`,
+        next: query.page < totalPages ? `${CLIENT_HOST}/rooms?limit=${query.limit}&page=${query.page + 1}` : '',
+        last: `${CLIENT_HOST}/rooms?limit=${query.limit}&page=${totalPages}`,
+      }
+    }
     
-//     await closeConnection();
-//     return {
-//       statusCode: 200,
-//       body: JSON.stringify(usersResponse)
-//     };
-//   } catch (e) {
-//     console.log(JSON.stringify(e))
-//     console.error(e)
-//     return {
-//       statusCode: 500,
-//       body: JSON.stringify({ message: 'Internal Server Error' })
-//     };
-//   }
-// }
+    await closeConnection();
+    return {
+      statusCode: 200,
+      body: JSON.stringify(roomsResponse)
+    };
+  } catch (e) {
+    console.log(JSON.stringify(e))
+    console.error(e)
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: 'Internal Server Error' })
+    };
+  }
+}
 
 interface RoomPostBodyRequest extends Partial<Room> {
   name: string;
@@ -140,7 +145,7 @@ export async function postRoomHandler(
   // All log statements are written to CloudWatch
   console.debug('Received event:', event);
   if (event.body === null) {
-    console.log("400 - POST /users - Body is null")
+    console.log("400 - POST /rooms - Body is null")
     return {
       statusCode: 400,
       body: JSON.stringify({ message: 'Bad Request' })
@@ -150,7 +155,7 @@ export async function postRoomHandler(
   // Validates body
   const requestBody = JSON.parse(event.body) as RoomPostBodyRequest;
   if (typeof requestBody.name !== 'string') {
-    console.log("400 - POST /users - Body is not valid")
+    console.log("400 - POST /rooms - Body is not valid")
     return {
       statusCode: 400,
       body: JSON.stringify({ message: 'Bad Request' })
@@ -160,7 +165,7 @@ export async function postRoomHandler(
   try {
     const mongoClient = await getMongoClient();
     if (!mongoClient || !mongoClient.db || !mongoClient.closeConnection) {
-      console.log("500 - POST /users - Error connecting to DB")
+      console.log("500 - POST /rooms - Error connecting to DB")
       return {
         statusCode: 500,
         body: JSON.stringify({ message: 'Internal Server Error' })
