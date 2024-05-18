f1ce57527ff70b57b7a7b579d4908187353b15c6
Jip Irfandy <irfandyjip98@gmail.com>
2024-05-18 12:05:09 +0700

    feat(api): add ability to add `users`

--- api/src/handlers/users.ts
+++ api/src/handlers/users.ts
@@ -3,6 +3,7 @@ import {
   APIGatewayProxyEvent,
   APIGatewayProxyResult
 } from 'aws-lambda';
+import { getMongoClient } from '../db/init';
 import { GetResponse } from '../lib/response';
 import { UserEntity } from '../models/user';
 
@@ -50,4 +51,62 @@ export const getUsersHandler = async (
     statusCode: 200,
     body: JSON.stringify(usersResponse)
   };
+}
+
+interface UserPostBodyRequest {
+  name: string;
+  email: string;
+}
+
+export async function postUserHandler(
+  event: APIGatewayProxyEvent,
+): Promise<APIGatewayProxyResult> {
+  // All log statements are written to CloudWatch
+  console.debug('Received event:', event);
+  if (event.body === null) {
+    console.log("400 - POST /users - Body is null")
+    return {
+      statusCode: 400,
+      body: JSON.stringify({ message: 'Bad Request' })
+    };
+  }
+
+  // Validates body
+  const requestBody = JSON.parse(event.body) as UserPostBodyRequest;
+  if (typeof requestBody.name !== 'string' || typeof requestBody.email !== 'string') {
+    console.log("400 - POST /users - Body is not valid")
+    return {
+      statusCode: 400,
+      body: JSON.stringify({ message: 'Bad Request' })
+    };
+  }
+
+  try {
+    const { db, closeConnection } = await getMongoClient();
+    console.log("Connection to DB established")
+
+    const usersCollection = db.collection('users');
+    const user: UserEntity = {
+      name: requestBody?.name,
+      email: requestBody?.email
+    }
+    const result = await usersCollection.insertOne(user);
+    const userBodyResponse: UserEntity = {
+      id: result.insertedId.toString(),
+      name: requestBody.name,
+      email: requestBody.email
+    }
+    await closeConnection();
+    return {
+      statusCode: 200,
+      body: JSON.stringify(userBodyResponse)
+    };
+  } catch (e) {
+    console.log(JSON.stringify(e))
+    console.error(e)
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: 'Internal Server Error' })
+    };
+  }
 }
