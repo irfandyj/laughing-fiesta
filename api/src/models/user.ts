fb34d31ec6de4c3684b88b27dcb4f8c3d614395d
Jip Irfandy <irfandyjip98@gmail.com>
2024-05-18 12:04:49 +0700

    feat(api): add a `User` model

--- api/src/models/user.ts
+++ api/src/models/user.ts
@@ -1,5 +1,15 @@
 export class UserEntity {
-  id: string;
+  id?: string;
   name: string;
   email: string;
+
+  constructor({ id, name, email }: {
+    id?: string;
+    name: string;
+    email: string;
+  }) {
+    this.id = id;
+    this.name = name;
+    this.email = email;
+  }
 }
