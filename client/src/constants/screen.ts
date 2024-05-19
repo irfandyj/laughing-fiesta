a52e5746cfb09e055295ed448502492fcc5a7694
Jip Irfandy <irfandyjip98@gmail.com>
2024-05-19 20:37:07 +0700

    feat(client): add a `Breakpoint` constant for reference

---
+++ client/src/constants/screen.ts
@@ -0,0 +1,12 @@
+/**
+ * Default Ant Design Breakpoints
+ * https://ant.design/components/layout
+ */
+export enum Breakpoints {
+  xs = '480px',
+  sm = '576px',
+  md = '768px',
+  lg = '992px',
+  xl = '1200px',
+  xxl = '1600px',
+}
