f7ca9681a67669fc406093aa0eaf057721df09d9
Jip Irfandy <irfandyjip98@gmail.com>
2024-05-19 19:50:57 +0700

    feat(clients): add unfinished layout

---
+++ client/src/components/templates/AuthenticatedLayout/AuthenticatedLayout.tsx
@@ -0,0 +1,46 @@
+import { UploadOutlined, UserOutlined, VideoCameraOutlined } from '@ant-design/icons';
+import { Layout, Menu } from 'antd';
+import React from 'react';
+
+const { Content, Sider } = Layout;
+
+const App: React.FC = (props) => {
+  const { children } = props;
+  return (
+    <Layout className='HomePage'>
+      <Sider
+        breakpoint="lg"
+        collapsedWidth="0"
+        onBreakpoint={broken => {
+          console.log(broken);
+        }}
+        onCollapse={(collapsed, type) => {
+          console.log(collapsed, type);
+        }}
+      >
+        <div className="logo" />
+        <Menu
+          theme="dark"
+          mode="inline"
+          defaultSelectedKeys={['4']}
+          items={[UserOutlined, VideoCameraOutlined, UploadOutlined, UserOutlined].map(
+            (icon, index) => ({
+              key: String(index + 1),
+              icon: React.createElement(icon),
+              label: `nav ${index + 1}`,
+            }),
+          )}
+        />
+      </Sider>
+      <Layout>
+        {/* <Header className="site-layout-sub-header-background" style={{ padding: 0 }} /> */}
+        <Content>
+          {children}
+        </Content>
+        {/* <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer> */}
+      </Layout>
+    </Layout>
+  );
+}
+
+export default App;
