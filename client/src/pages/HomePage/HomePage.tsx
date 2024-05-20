b3cc82ea711efa14de446c6d251e1ea696395edf
Jip Irfandy <irfandyjip98@gmail.com>
2024-05-21 00:13:04 +0700

    feat(client): connect dva with `HomePage` to display username!

--- client/src/pages/HomePage/HomePage.tsx
+++ client/src/pages/HomePage/HomePage.tsx
@@ -1,14 +1,24 @@
 import AuthenticatedLayout from '@/components/templates/AuthenticatedLayout';
 import './HomePage.module.css';
+import { connect } from 'umi';
+import { ProfileModelState } from '@/models/profile/profile.types';
 
-const HomePage: React.FC = (props) => {
+interface HomePageProps {
+  profile: ProfileModelState
+}
+
+const HomePage: React.FC<HomePageProps> = (props) => {
+  const { profile } = props;
+
   return (
     <AuthenticatedLayout>
       <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
-        Welcome, You!
+        Welcome, You, {profile.profiles[profile.currentChosenIndexProfile].username}!
       </div>
     </AuthenticatedLayout>
   );
 }
 
-export default HomePage;
+export default connect(({ profile }: { profile: ProfileModelState }) => {
+  return { profile }
+})(HomePage)
