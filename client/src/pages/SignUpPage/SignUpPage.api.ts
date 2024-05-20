efe857fb58f443bc5efcf51d41f8ecada9ba1484
Jip Irfandy <irfandyjip98@gmail.com>
2024-05-20 20:57:40 +0700

    feat(client): set profile in `signUpFormFlow`

--- client/src/pages/SignUpPage/SignUpPage.api.ts
+++ client/src/pages/SignUpPage/SignUpPage.api.ts
@@ -1,7 +1,9 @@
 import { api, replaceAuthorizationHeader } from '@/lib/axios';
+import { PROFILE_ACTIONS } from '@/models/profile/profile.constants';
 import { AxiosResponse } from 'axios';
 import { Endpoints as ApiEndpoints } from 'gigradar-commons/build/constants/endpoints';
 import { SignUpDto, UserAuthenticationDto } from 'gigradar-commons/build/dtos/authentication';
+import { getDvaApp } from 'umi';
 
 type SignUpResponse = AxiosResponse<UserAuthenticationDto>;
 export async function signUp(signupData: SignUpDto) {
@@ -22,6 +24,10 @@ export async function signUpFormFlow(data: SignUpDto) {
     replaceAuthorizationHeader(signUpRes.data.token);
 
     // Set personal data in app state
+    getDvaApp()._store.dispatch({
+      type: PROFILE_ACTIONS.ADD_PROFILE,
+      payload: signUpRes.data
+    });
     
     // Connect to a websocket
     // Do websocket flow
